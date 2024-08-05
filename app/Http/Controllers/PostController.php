42f4996d5da6345beb69625eb0b81187be283a1b
Ayush Mahatara <ayush.mahatara10@gmail.com>
2024-08-06 02:59:09 +0545

    post show component

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,9 +14,19 @@ class PostController extends Controller
             'posts.index',
             [
                 'categories' => Category::whereHas('posts', function ($query) {
-                        $query->published();
-                    })
+                    $query->published();
+                })
                     ->take(10)->get()
+            ]
+        );
+    }
+
+    public function show(Post $post)
+    {
+        return view(
+            'posts.show',
+            [
+                'post' => $post
             ]
         );
     }
