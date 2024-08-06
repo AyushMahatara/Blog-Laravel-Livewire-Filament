e457b73832921748526599ac6e483010ffc20107
Ayush Mahatara <ayush.mahatara10@gmail.com>
2024-08-06 12:35:54 +0545

    post comment

--- app/Models/User.php
+++ app/Models/User.php
@@ -73,6 +73,11 @@ class User extends Authenticatable
         return $this->belongsToMany(Post::class, 'post_like')->withTimestamps();
     }
 
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
     public function hasLiked($post)
     {
         return $this->likes()->where('post_id', $post->id)->exists();
